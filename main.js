56440cf5e236da5565528bc3b212676b2b92ceac
Juan122113 <ammiel89@hotmail.com>
2024-07-20 19:24:30 -0300

    Update index.html styles.css Created main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+let button = document.getElementById("btn");
+let shareCtn = document.getElementById("sharectn");
+let shareIcon = document.getElementById("share-icon");
+
+button.addEventListener('click', () => {
+    if (shareCtn.style.display != "flex") {
+        button.style.backgroundColor = "var(--very-dark-grayish-blue)";
+        shareCtn.style.display = "flex";
+        // shareCtn.style.pointerEvents = "auto";
+        shareIcon.style.fill = "white";
+    } else {
+        button.style.backgroundColor = "var(--light-grayish-blue)";
+        shareCtn.style.display = "none";
+        // shareCtn.style.pointerEvents = "none";
+        shareIcon.style.fill = "var(--desaturated-dark-blue)";
+        }
+    
+})
