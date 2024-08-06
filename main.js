d2ece6fe0d026e58e5920ae9633ba54296343986
Juan122113 <ammiel89@hotmail.com>
2024-08-06 12:23:27 -0300

    Update styles.css & main.js

--- main.js
+++ main.js
@@ -7,27 +7,11 @@ button.addEventListener('click', () => {
     if (shareCtn.style.display != "flex") {
         button.style.backgroundColor = "var(--desaturated-dark-blue)";
         shareCtn.style.display = "flex";
-        // shareCtn.style.pointerEvents = "auto";
         shareIcon.style.fill = "white";
     } else {
         button.style.backgroundColor = "var(--light-grayish-blue)";
         shareCtn.style.display = "none";
-        // shareCtn.style.pointerEvents = "none";
         shareIcon.style.fill = "var(--desaturated-dark-blue)";
         }
     
 })
-
-// body.addEventListener('click', () => {
-//     if (shareCtn.style.display == "flex") {
-//         button.style.backgroundColor = "var(--light-grayish-blue)";
-//         shareCtn.style.display = "none";
-//         // shareCtn.style.pointerEvents = "none";
-//         shareIcon.style.fill = "var(--desaturated-dark-blue)";
-//     }
-// })
-
-// document.getElementById('clickable-link').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     window.location.href = this.href;
-// })
