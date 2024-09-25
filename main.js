cfe7f564832ddb07ce0bc9cad01b2130cd27c6a6
Juan122113 <ammiel89@hotmail.com>
2024-09-25 08:22:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,33 +2,11 @@ const button = document.getElementById("button");
 const shareCtn = document.getElementById("sharectn");
 const shareIcon = document.getElementById("share-icon");
 
-const elements = {
-    button: "backgroundColor",
-    shareCtn: "display",
-    shareIcon: "fill"
-};
-
-const elementsKeys = Object.keys(elements);
-
-console.log(elementsKeys);
-
-const handleClick = (e) => {
-    elementsKeys.forEach(element => {
-        if (shareCtn.style.display != "flex") {
-            // element.style.elements[element] =
-        }
-    })
-}
-
 button.addEventListener('click', () => {
-    if (shareCtn.style.display != "flex") {
-        button.style.backgroundColor = "var(--desaturated-dark-blue)";
-        shareCtn.style.display = "flex";
-        shareIcon.style.fill = "white";
-    } else {
-        button.style.backgroundColor = "var(--light-grayish-blue)";
-        shareCtn.style.display = "none";
-        shareIcon.style.fill = "var(--desaturated-dark-blue)";
-        }
+    const isVisible = shareCtn.style.display === "flex";
+
+    button.style.backgroundColor = isVisible ? "var(--light-grayish-blue)" : "var(--desaturated-dark-blue)";
+    shareCtn.style.display = isVisible ? "none" : "flex";
+    shareIcon.style.fill = isVisible ? "var(--desaturated-dark-blue)" : "white";
     
-})
+});
