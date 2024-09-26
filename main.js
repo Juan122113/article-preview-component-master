9aaee31d98857dad2d6d9938dc4683e585333514
Juan122113 <ammiel89@hotmail.com>
2024-09-26 18:08:55 -0300

    Update index.html & main.js

--- main.js
+++ main.js
@@ -2,11 +2,25 @@ const button = document.getElementById("button");
 const shareCtn = document.getElementById("sharectn");
 const shareIcon = document.getElementById("share-icon");
 
-button.addEventListener('click', () => {
-    const isVisible = shareCtn.style.display === "flex";
-
+function handleButton(button, isVisible) {
     button.style.backgroundColor = isVisible ? "var(--light-grayish-blue)" : "var(--desaturated-dark-blue)";
-    shareCtn.style.display = isVisible ? "none" : "flex";
+}
+
+function handleShareCtn(share, isVisible) {
+    share.style.display = isVisible ? "none" : "flex";
+}
+
+function handleShareIcon(shareIcon, isVisible) {
     shareIcon.style.fill = isVisible ? "var(--desaturated-dark-blue)" : "white";
-    
-});
+}
+
+function handleEvent() {
+    const isVisible = shareCtn.style.display === "flex";
+    console.log(isVisible);
+
+    handleButton(button, isVisible);
+    handleShareCtn(shareCtn, isVisible)
+    handleShareIcon(shareIcon, isVisible)
+}
+
+button.addEventListener('click', handleEvent);
