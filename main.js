66d437593accdaaab8c1021651e60ca377e521a0
Juan122113 <ammiel89@hotmail.com>
2024-10-23 12:21:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Definición de estilos
 const styles = {
     visible: {
         button: "var(--light-grayish-blue)",
@@ -18,20 +17,6 @@ const elements = {
     shareIcon: document.getElementById("shareicon")
 };
 
-// const isVisible = elements.shareCtn.style.display === "flex";
-
-// document.addEventListener('DOMContentLoaded', isVisible);
-
-// elements.button.addEventListener('click', () => updateElementStyles(elements, styles, styleAttributes));
-
-// function initializeElements() {
-//     return {
-//         button: document.getElementById("button"),
-//         shareCtn: document.getElementById("sharectn"),
-//         shareIcon: document.getElementById("shareicon")
-//     };
-// }
-
 const styleAttributes = {
     button: "backgroundColor",
     shareCtn: "display",
@@ -39,19 +24,8 @@ const styleAttributes = {
 };
 
 function updateElementStyles(elements, styleAttributes, currentStyle) {
-    // const isVisible = elements.shareCtn.style.display === "flex";
-    // console.log(isVisible);
-    
-    // const currentStyle = isVisible ? styles.visible : styles.hidden;
-    console.log(currentStyle);
-    // elements.button.style.backgroundColor = currentStyle.button;
-    // elements.shareCtn.style.display = currentStyle.shareCtn;
-    // elements.shareIcon.style.fill = currentStyle.shareIcon;
-
     for (let element in elements) {
-        //for (let atrs in atr) {
         elements[element].style[styleAttributes[element]] = currentStyle[element];
-        //}
     }
 }
 
@@ -66,23 +40,3 @@ function toggleShareVisibility(elements, styles, styleAttributes) {
 }
 
 elements.button.addEventListener('click', () => toggleShareVisibility(elements, styles, styleAttributes));
-
-
-// function setupEventListeners(elements, styles, styleAttributes) {
-//     elements.button.addEventListener('click', () => toggleShareVisibility(elements, styles, styleAttributes));
-// }
-
-// setupEventListeners(elements, styles, styleAttributes);
-
-// function setupEventListeners(elements) {
-//     elements.button.addEventListener('click', () => toggleShareVisibility(elements));
-// }
-
-// setupEventListeners(elements);
-
-// function initializeApp() {
-//     const elements = initializeElements();
-//     setupEventListeners(elements);
-// }
-
-// initializeApp();
